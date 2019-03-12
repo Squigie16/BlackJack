c260fc050f15e32eb1edcbee167ec93e9531d057
agost <agost@192.168.0.20>
2019-03-11 23:34:30 -0400

    Test Commit

--- BlackJack/src/blackjack/Card.java
+++ BlackJack/src/blackjack/Card.java
@@ -2,6 +2,7 @@
  * SYST 17796 Project Winter 2019 Base code.
  * Students can modify and extend to implement their game.
  * Add your name as a modifier and the date!
+ * Test comment to practice for Exam!
  */
 package blackjack;
 
